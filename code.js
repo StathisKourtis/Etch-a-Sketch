96ba94f6b6394bbc9d8da4b700559dae40d54943
KremasLagos <stathxmkourtis@gmail.com>
2025-05-30 15:56:30 +0300

    Html stracture , grid creation function with number of rows and collums are input from the user

---
+++ code.js
@@ -0,0 +1,46 @@
+const container = document.querySelector(".container")
+let i = 0
+let y = 0
+let row = 16
+let collum = 16
+const box = []
+const rowArr = []
+function creareGrid(row , collum){
+
+    for (i = 0; i < row ; i++){
+        rowArr[i] = document.createElement("div")
+        rowArr[i].classList.add("row")
+        container.appendChild(rowArr[i])
+        rowArr[i].style.display = "flex"
+    
+        for (y = 1; y < collum; y++){
+            box[y] = document.createElement("div")
+            box[y].classList.add("box")
+            box[y].style.border = "solid black 1px"
+            box[y].style.height = "10px"
+            box[y].style.width = "10px"
+            box[y].style.aspectRatio = "1 / 1"
+            // box[y].style.flex = "1 1 auto"
+            rowArr[i].appendChild(box[y])
+            
+        }   
+    }
+}
+function returnGrid(){
+    cleanGrid()
+    row = document.getElementById("row").value
+    collum = document.getElementById("collum").value
+    creareGrid(row , collum)
+}
+function cleanGrid() {
+   let boxRemove = document.querySelectorAll(".box")
+   let rowRemove = document.querySelectorAll("row")
+
+   boxRemove.forEach(box => {
+    box.remove()
+    
+   });
+   rowRemove.forEach(row => {
+    row.remove
+   });
+}
